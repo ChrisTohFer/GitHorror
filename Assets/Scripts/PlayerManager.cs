57797ca8cc8e1ee5daf97a01feb1c8a212c219ae
Chris <chrisrkemp@hotmail.co.uk>
2021-02-01 16:56:25 +0000

    Added ability to set starting bolts/medkits

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -11,10 +11,15 @@ public class PlayerManager : MonoBehaviour
     public FirstPersonAIO FP;
     public float CamShakeTime = 0.1f;
     public float CamShakeIntensity = 0.05f;
+    public int StartingBolts = 6;
+    public int StartingMedKits = 0;
 
     private void Awake()
     {
         Singleton = this;
+        PlayerStats.Reset();
+        PlayerStats.SetStat("bolts", StartingBolts);
+        PlayerStats.SetStat("medkits", StartingMedKits);
     }
 
     public void TakeDamage(float damage)
