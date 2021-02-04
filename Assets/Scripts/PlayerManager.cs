e9c606d74e1da8770a9de97af9cc9745feb912bc
Lawrence Shahid <lawshahid@gmail.com>
2021-02-04 12:56:16 +0000

    Added TextMeshPro, Minor item placement adjustments, Started work on UI

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -14,6 +15,16 @@ public class PlayerManager : MonoBehaviour
     public int StartingBolts = 6;
     public int StartingMedKits = 0;
     public float MedKitHeal = 50f;
+
+    // Law Code (Start)
+    public TextMeshProUGUI UI_Health;
+
+    public GameObject UI_Kit1;
+    public GameObject UI_Kit2;
+    public GameObject UI_Kit3;
+    public GameObject UI_Kit4;
+    public GameObject UI_Kit5;
+    // Law Code (End)
 
     private void Awake()
     {
@@ -40,6 +51,84 @@ public class PlayerManager : MonoBehaviour
             AudioManager.PlayOnPlayer(AudioManager.AudioClips.PlayerHeal);
 
         }
+
+        // Law Code (Start)
+        //Medkit UI
+        if (PlayerStats.GetStat("medkits") <= 0)
+        {
+            UI_Kit1.SetActive(false);
+            UI_Kit2.SetActive(false);
+            UI_Kit3.SetActive(false);
+            UI_Kit4.SetActive(false);
+            UI_Kit5.SetActive(false);
+        }
+
+        if (PlayerStats.GetStat("medkits") == 1)
+        {
+            UI_Kit1.SetActive(true);
+            UI_Kit2.SetActive(false);
+            UI_Kit3.SetActive(false);
+            UI_Kit4.SetActive(false);
+            UI_Kit5.SetActive(false);
+        }
+
+        if (PlayerStats.GetStat("medkits") == 2)
+        {
+            UI_Kit1.SetActive(true);
+            UI_Kit2.SetActive(true);
+            UI_Kit3.SetActive(false);
+            UI_Kit4.SetActive(false);
+            UI_Kit5.SetActive(false);
+        }
+
+        if (PlayerStats.GetStat("medkits") == 3)
+        {
+            UI_Kit1.SetActive(true);
+            UI_Kit2.SetActive(true);
+            UI_Kit3.SetActive(true);
+            UI_Kit4.SetActive(false);
+            UI_Kit5.SetActive(false);
+        }
+
+        if (PlayerStats.GetStat("medkits") == 4)
+        {
+            UI_Kit1.SetActive(true);
+            UI_Kit2.SetActive(true);
+            UI_Kit3.SetActive(true);
+            UI_Kit4.SetActive(true);
+            UI_Kit5.SetActive(false);
+        }
+
+        if (PlayerStats.GetStat("medkits") == 5)
+        {
+            UI_Kit1.SetActive(true);
+            UI_Kit2.SetActive(true);
+            UI_Kit3.SetActive(true);
+            UI_Kit4.SetActive(true);
+            UI_Kit5.SetActive(true);
+        }
+
+        //Health UI
+        if (PlayerStats.GetStat("health") == 0f)
+        {
+            UI_Health.SetText("DEAD");
+        }
+
+        if (PlayerStats.GetStat("health") > 0f && PlayerStats.GetStat("health") <= 30f)
+        {
+            UI_Health.SetText("DANGER");
+        }
+
+        if (PlayerStats.GetStat("health") > 30f && PlayerStats.GetStat("health") <= 70f)
+        {
+            UI_Health.SetText("CAUTION");
+        }
+
+        if (PlayerStats.GetStat("health") > 70f && PlayerStats.GetStat("health") <= 100f)
+        {
+            UI_Health.SetText("FINE");
+        }
+        // Law Code (End)
     }
 
     public void TakeDamage(float damage)
